cf24aba781258aa2db4fa18f53f1fc8999757686
Flakes WRLD <71187213+FristyFlakes@users.noreply.github.com>
2020-11-25 13:23:29 +0100

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -202,13 +202,13 @@ if __name__ == "__main__" :
     #
     upload_as_doc_handler = MessageHandler(
         upload_as_doc,
-        filters=Filters.command("uploadasdoc@Renamesplitbot") & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command("uploadasdoc@YoutubeZoneBot") & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_doc_handler)
     #
     upload_as_video_handler = MessageHandler(
         upload_as_video,
-        filters=Filters.command("uploadasvideo@Renamesplitbot") & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command("uploadasvideo@YoutubeZoneBot") & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_video_handler)
 
